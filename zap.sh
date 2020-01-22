9eea16ca9d0ba45c52d84c23757f0e7ea43edee9
nu11secur1ty <venvaropt@gmail.com>
2020-01-22 14:15:36 -0500

    Update zap.sh

--- zap.sh
+++ zap.sh
@@ -3,6 +3,4 @@ cd /opt/
   wget https://github.com/zaproxy/zaproxy/releases/download/v2.9.0/ZAP_2_9_0_unix.sh
   chmod a+x ZAP_2_9_0_unix.sh
     ./ZAP_2_9_0_unix.sh
-  sleep 5
-    rm ZAP_2_9_0_unix.sh
 exit 0;
