cad6636edc773f863de4c7bcd9b9ab92959def4c
nu11secur1ty <venvaropt@gmail.com>
2020-04-20 11:25:45 +0300

    Update installer.sh

--- Latest/installer.sh
+++ Latest/installer.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
 # author @nu11secur1ty 2020
 cd /opt/
-  wget https://github.com/zaproxy/zaproxy/releases/download/v2.9.0/ZAP_2_9_0_unix.sh
+  wget https://github.com/zaproxy/zaproxy/releases/download/w2020-04-14/ZAP_WEEKLY_D-2020-04-14.zip
+  unzip ZAP_WEEKLY_D-2020-04-14.zip
+  
   chmod a+x ZAP_2_9_0_unix.sh
     ./ZAP_2_9_0_unix.sh
 exit 0;
