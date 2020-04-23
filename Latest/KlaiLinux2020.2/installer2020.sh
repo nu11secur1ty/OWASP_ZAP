59ef6e7ceca22ad65cb5f859c0b67d6398a4e507
nu11secur1ty <venvaropt@gmail.com>
2020-04-23 18:54:55 +0300

    Update installer2020.sh

--- Latest/KlaiLinux2020.2/installer2020.sh
+++ Latest/KlaiLinux2020.2/installer2020.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 # author @nu11secur1ty 2020
+curl -s https://raw.githubusercontent.com/nu11secur1ty/Kali-Linux/master/JAVA-INSTALLER-SILENT-MOD/javas1lent.sh | bash
 cd /opt/
-  sudo wget https://github.com/zaproxy/zaproxy/releases/download/w2020-04-21/ZAP_WEEKLY_D-2020-04-21.zip
-  sudo unzip ZAP_WEEKLY_D-2020-04-21.zip
+  wget https://github.com/zaproxy/zaproxy/releases/download/w2020-04-21/ZAP_WEEKLY_D-2020-04-21.zip
+  unzip ZAP_WEEKLY_D-2020-04-21.zip
       cd
-    sudo ln -s /opt/ZAP_WEEKLY_D-2020-04-21/zap.sh /usr/bin/zapnu11secur1ty
+    ln -s /opt/ZAP_WEEKLY_D-2020-04-21/zap.sh /usr/bin/zapnu11secur1ty
 exit 0;
