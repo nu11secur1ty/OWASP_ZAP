3f83845f425b452dec7d20d14d6d35f1744ad7cb
nu11secur1ty <venvaropt@gmail.com>
2020-04-23 19:05:25 +0300

    Update installer2020.sh

--- Latest/KlaiLinux2020.2/installer2020.sh
+++ Latest/KlaiLinux2020.2/installer2020.sh
@@ -4,6 +4,6 @@ curl -s https://raw.githubusercontent.com/nu11secur1ty/Kali-Linux/master/JAVA-IN
 cd /opt/
   wget https://github.com/zaproxy/zaproxy/releases/download/w2020-04-21/ZAP_WEEKLY_D-2020-04-21.zip
   unzip ZAP_WEEKLY_D-2020-04-21.zip
-      cd
-    ln -s /opt/ZAP_WEEKLY_D-2020-04-21/zap.sh /usr/bin/zapnu11secur1ty
+      cd /root/
+    ln -s /opt/ZAP_D-2020-04-21/zap.sh /usr/bin/zapnu11secur1ty
 exit 0;
