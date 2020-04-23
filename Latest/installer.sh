716e7fb9a5c6b4f81bd6bcb75d10def5b604acdb
nu11secur1ty <venvaropt@gmail.com>
2020-04-23 19:19:01 +0300

    Delete installer.sh

--- Latest/installer.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-# author @nu11secur1ty 2020
-cd /opt/
-  wget https://github.com/zaproxy/zaproxy/releases/download/w2020-04-21/ZAP_WEEKLY_D-2020-04-21.zip
-  unzip ZAP_WEEKLY_D-2020-04-21.zip
-      cd
-    ln -s /opt/ZAP_WEEKLY_D-2020-04-21/zap.sh /usr/bin/zapnu11secur1ty
-exit 0;
