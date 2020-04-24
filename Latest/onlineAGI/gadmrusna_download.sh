552634848aabed4244fa344a781af35d2eb66594
nu11secur1ty <venvaropt@gmail.com>
2020-04-24 23:35:12 +0300

    Update gadmrusna_download.sh

--- Latest/onlineAGI/gadmrusna_download.sh
+++ Latest/onlineAGI/gadmrusna_download.sh
@@ -9,6 +9,6 @@ echo "Please wait..."
   sleep 7;
   echo -e "\e[31mAfter you get the automation AGI program\e[0m"
   echo -e "\e[31mPlease execute:\e[0m"
-  echo -e "\e[35mperl /home/gadmrusna.pl\e[0m"
+  echo -e "\e[35mperl gadmrusna.pl\e[0m"
   echo -e "\e[31mFollow the instruction =)\e[0m"
 exit 0
