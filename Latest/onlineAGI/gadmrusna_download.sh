db917271669602695a71ab528233eb8853aa4d1c
nu11secur1ty <venvaropt@gmail.com>
2020-04-24 22:27:49 +0300

    Update gadmrusna_download.sh

--- Latest/onlineAGI/gadmrusna_download.sh
+++ Latest/onlineAGI/gadmrusna_download.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 sudo rm -rf gadmrusna.pl
 cd /home/
-  rm -rf *zapnu11secur1ty.xml*
+  sudo rm -rf *zapnu11secur1ty.xml*
     cd 
   sleep 3;
 sudo wget https://raw.githubusercontent.com/nu11secur1ty/OWASP_ZAP/master/Latest/onlineAGI/gadmrusna.pl
