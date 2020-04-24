614082011f5c9f142d179bd5db9e212785322850
nu11secur1ty <venvaropt@gmail.com>
2020-04-25 01:18:39 +0300

    Update gadmrusna.pl

--- Latest/onlineAGI/gadmrusna.pl
+++ Latest/onlineAGI/gadmrusna.pl
@@ -3,12 +3,13 @@
 use strict;
 use warnings;
 use diagnostics;
+$datestring = localtime();
 
 print "Give the URL...\n";
 chomp (my $URL_ = <STDIN>);
-my $zapnu11secur1ty_killer_auto = `sudo zapnu11secur1ty -quickurl $URL_ -quickout /home/zapnu11secur1ty.xml`;
+my $zapnu11secur1ty_killer_auto = `sudo zapnu11secur1ty -quickurl $URL_ -quickout /home/zapnu11secur1ty_$datestring.xml`;
 # Working...
 #  sleep 3;
 #  my $killer_tuaka_prosti = `kill $(pgrep -f zapnu11secur1ty)`;
-              my $chr0m_ = `google-chrome /home/zapnu11secur1ty.xml`;
+              my $chr0m_ = `google-chrome /home/zapnu11secur1ty_$datestring.xml`;
         exit 0;
