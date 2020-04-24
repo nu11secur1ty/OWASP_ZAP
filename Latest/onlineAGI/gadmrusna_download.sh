990e9e9b99d44e136cc346e1c1a22071d2f724b3
nu11secur1ty <venvaropt@gmail.com>
2020-04-24 22:44:43 +0300

    Update gadmrusna_download.sh

--- Latest/onlineAGI/gadmrusna_download.sh
+++ Latest/onlineAGI/gadmrusna_download.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
+# @nu11secur1ty
 sudo rm -rf *gadmrusna.pl*
 sudo rm -rf *zapnu11secur1ty.xml*
-sleep 3;
+  sleep 3;
 sudo wget https://raw.githubusercontent.com/nu11secur1ty/OWASP_ZAP/master/Latest/onlineAGI/gadmrusna.pl
 echo "Please wait..."
   sleep 7;
