122b5d23e5551a5ec80199553a1209c22a983310
nu11secur1ty <venvaropt@gmail.com>
2020-04-24 20:08:15 +0300

    Update gadmrusna.pl

--- Latest/gadmrusna.pl
+++ Latest/gadmrusna.pl
@@ -2,6 +2,6 @@
 # @nu11secur1ty
 print "Give the url...\n";
 chomp (my $URL_target = <STDIN>);
-my $zapnu11secur1ty_killer_auto = `zapnu11secur1ty -quickurl $URL_target -quickout $URL_target.xml`;
+chomp (my $zapnu11secur1ty_killer_auto = `sudo zapnu11secur1ty -quickurl $URL_target -quickout /home/nu11secur1ty/$URL_target.xml`);
   print "$zapnu11secur1ty\n";
 system($zapnu11secur1ty)
