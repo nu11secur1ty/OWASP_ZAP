76e2610537f2eb911429154bfe9135577b6a5c4d
nu11secur1ty <venvaropt@gmail.com>
2020-04-24 21:41:51 +0300

    Update gadmrusna.pl

--- Latest/onlineAGI/gadmrusna.pl
+++ Latest/onlineAGI/gadmrusna.pl
@@ -4,10 +4,8 @@ use strict;
 use warnings;
 use diagnostics;
 
-print "Give the url...\n";
-chomp (my $URL_target = <STDIN>);
-        my $zapnu11secur1ty_killer_auto = `sudo zapnu11secur1ty -quickurl $URL_target -quickout /home/nu11secur1ty/zapnu11secur1ty.xml`;
+my $zapnu11secur1ty_killer_auto = `sudo zapnu11secur1ty -quickurl @ARGV -quickout /home/nu11secur1ty/zapnu11secur1ty.xml`;
         system($zapnu11secur1ty_killer_auto);
         my $chr0m_ = `google-chrome zapnu11secur1ty.xml`;
-        system($chr0m_);
+                system($chr0m_);
 exit 0;
