a6439c42e7503208cab8e5b8eb4c41d4b14db09c
nu11secur1ty <venvaropt@gmail.com>
2020-04-24 21:11:39 +0300

    Update gadmrusna.pl

--- Latest/gadmrusna.pl
+++ Latest/gadmrusna.pl
@@ -1,7 +1,13 @@
 #!/usr/bin/perl
 # @nu11secur1ty
+use strict;
+use warnings;
+use diagnostics;
+
 print "Give the url...\n";
 chomp (my $URL_target = <STDIN>);
-chomp (my $zapnu11secur1ty_killer_auto = `sudo zapnu11secur1ty -quickurl $URL_target -quickout /home/nu11secur1ty/$URL_target.xml`);
-  print "$zapnu11secur1ty\n";
-system($zapnu11secur1ty)
+        my $zapnu11secur1ty_killer_auto = `sudo zapnu11secur1ty -quickurl $URL_target -quickout /home/nu11secur1ty/zapnu11secur1ty.xml`;
+        system($zapnu11secur1ty_killer_auto);
+        my $chr0m_ = `google-chrome zapnu11secur1ty.xml`;
+        system($chr0m_);
+exit 0;
