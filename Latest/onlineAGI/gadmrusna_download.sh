f5415c5cfa9cd478c0166a3a7e6e5a97f5d50a3a
nu11secur1ty <venvaropt@gmail.com>
2020-04-24 21:48:27 +0300

    Update gadmrusna_download.sh

--- Latest/onlineAGI/gadmrusna_download.sh
+++ Latest/onlineAGI/gadmrusna_download.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
+sudo rm -rf gadmrusna.pl
+  sleep 3;
 sudo wget https://raw.githubusercontent.com/nu11secur1ty/OWASP_ZAP/master/Latest/onlineAGI/gadmrusna.pl
 echo "Please wait..."
-  sleep 15;
+  sleep 7;
   echo -e "\e[31mAfter you get the automation AGI program\e[0m"
   echo -e "\e[31mPlease execute: perl gadmrusna.pl\e[0m"
   echo -e "\e[31mFollow the instruction =)\e[0m"
