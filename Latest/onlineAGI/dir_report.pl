4c523d71444f813f5154f118b12dc22112f87d63
nu11secur1ty <venvaropt@gmail.com>
2020-04-26 02:37:26 +0300

    Create dir_report.pl

---
+++ Latest/onlineAGI/dir_report.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+# @nu11secur1ty
+use strict;
+use warnings;
+use diagnostics;
+
+my $dt = time();
+my $check = 1;
+        if ($check > 0 ){
+                my $dir = `mkdir report.$dt`;
+        }
