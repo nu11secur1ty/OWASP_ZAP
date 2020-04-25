065ecffd87ac1d6b7b93d67c570705b2ab726b14
nu11secur1ty <venvaropt@gmail.com>
2020-04-25 09:41:39 +0300

    Update gadmrusna.pl

--- Latest/onlineAGI/gadmrusna.pl
+++ Latest/onlineAGI/gadmrusna.pl
@@ -8,11 +8,11 @@ print "The gathering begins at $datestring......\n\n\n";
 
 print "Give the URL...\n";
 chomp (my $URL_ = <STDIN>);
-my $zapnu11secur1ty_killer_auto = `sudo zapnu11secur1ty -quickurl $URL_ -quickout /home/zap.xml`;
+my $zapnu11secur1ty_killer_auto = `sudo zapnu11secur1ty -quickurl $URL_ -quickout /home/zapcontainer/zap.xml`;
 # Working...
 #  sleep 3;
 #  my $killer_tuaka_prosti = `kill $(pgrep -f zapnu11secur1ty)`;
-                my $move = `sudo mv /root/.html /home/`;
-                  my $chr0m_ = `google-chrome /home/zap.xml`;
-                  my $chr0m_2 = `google-chrome /home/.html`;
+                my $move = `sudo mv /root/.html /home/zapcontainer/`;
+                  my $chr0m_ = `google-chrome /home/zapcontainer/zap.xml`;
+                  my $chr0m_2 = `google-chrome /home/zapcontainer/.html`;
         exit 0;
