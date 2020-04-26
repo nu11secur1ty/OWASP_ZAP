2f2fb103a0d8f01e9637b9c2226e1d994cf99051
nu11secur1ty <venvaropt@gmail.com>
2020-04-26 11:07:36 +0300

    Update gadmrusna.pl

--- Latest/onlineAGI/gadmrusna.pl
+++ Latest/onlineAGI/gadmrusna.pl
@@ -10,21 +10,23 @@ my $check = 1;
                 chomp (my $dir = `sudo mkdir -p /home/zapcontainer/report.$dt`);
                 system($dir);
         }
-        
-my $dti = localtime();
-print "The gathering begins at $dti......\n\n\n";
+
+        my $dti = localtime();
+
+        print "The gathering begins at $dti......\n\n\n";
 
 print "Give the URL...\n";
 chomp (my $URL_ = <STDIN>);
-my $zapnu11secur1ty_killer_auto = `sudo zapnu11secur1ty -quickurl $URL_ -quickout /home/zapcontainer/report.$dt/zap.xml`;
-### Working...
+chomp (my $dir = `sudo mkdir -p /home/zapcontainer/report.$dt$URL_`);
+my $zapnu11secur1ty_killer_auto = `sudo zapnu11secur1ty -quickurl $URL_ -quickout /home/zapcontainer/report.$dt$URL_/zap.xml`;
+# Working...
 #  sleep 3;
 #  my $killer_tuaka_prosti = `kill $(pgrep -f zapnu11secur1ty)`;
 
         #################################################################
-                my $move = `sudo mv /root/.html /home/zapcontainer/report.$dt/`;
-                my $chr0m_ = `google-chrome /home/zapcontainer/report.$dt/zap.xml`;
-                my $chr0m_2 = `google-chrome /home/zapcontainer/report.$dt/.html`;
+                my $move = `sudo mv /root/.html /home/zapcontainer/report.$dt$URL_/`;
+                my $chr0m_ = `google-chrome /home/zapcontainer/report.$dt$URL_/zap.xml`;
+                my $chr0m_2 = `google-chrome /home/zapcontainer/report.$dt$URL_/.html`;
         
         #------------------------------------------------------------#
                # my $move = `sudo mv /root/.html /home/zapcontainer/`;
