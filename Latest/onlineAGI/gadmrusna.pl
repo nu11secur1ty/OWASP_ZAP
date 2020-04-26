1ac7be09c8348d027538d4f7f9d622b594a309d4
nu11secur1ty <venvaropt@gmail.com>
2020-04-26 10:01:34 +0300

    Update gadmrusna.pl

--- Latest/onlineAGI/gadmrusna.pl
+++ Latest/onlineAGI/gadmrusna.pl
@@ -12,7 +12,20 @@ my $zapnu11secur1ty_killer_auto = `sudo zapnu11secur1ty -quickurl $URL_ -quickou
 # Working...
 #  sleep 3;
 #  my $killer_tuaka_prosti = `kill $(pgrep -f zapnu11secur1ty)`;
-                my $move = `sudo mv /root/.html /home/zapcontainer/`;
-                  my $chr0m_ = `google-chrome /home/zapcontainer/zap.xml`;
-                  my $chr0m_2 = `google-chrome /home/zapcontainer/.html`;
+my $check = 1;
+        if ($check > 0 ){
+                chomp (my $dir = `mkdir -p report.$dt`);
+                system($dir);
+        }
+        #################################################################
+                my $move = `sudo mv /root/.html /home/$dt/`;
+                my $chr0m_ = `google-chrome /home/$dt/zap.xml`;
+                my $chr0m_2 = `google-chrome /home/$dt/.html`;
+        
+        #------------------------------------------------------------#
+               # my $move = `sudo mv /root/.html /home/zapcontainer/`;
+               #  my $chr0m_ = `google-chrome /home/zapcontainer/zap.xml`;
+               # my $chr0m_2 = `google-chrome /home/zapcontainer/.html`;
+                  my $dt = time();
+
         exit 0;
