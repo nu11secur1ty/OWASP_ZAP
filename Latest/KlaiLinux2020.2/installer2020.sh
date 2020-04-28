5ae00bb6f45fa627998ec529b7669f724d250937
nu11secur1ty <venvaropt@gmail.com>
2020-04-28 13:46:37 +0300

    Update installer2020.sh

--- Latest/KlaiLinux2020.2/installer2020.sh
+++ Latest/KlaiLinux2020.2/installer2020.sh
@@ -3,12 +3,12 @@
 # remove before installing the latest version
 
 cd /opt/
-sudo rm -rf /opt/*ZAP_*
-sudo rm -rf /usr/bin/zapnu11secur1ty
+rm -rf /opt/*ZAP_*
+rm -rf /usr/bin/zapnu11secur1ty
   sleep 3;
 # cd /opt/
-  sudo wget https://github.com/zaproxy/zaproxy/releases/download/w2020-04-21/ZAP_WEEKLY_D-2020-04-21.zip
-  sudo unzip ZAP_WEEKLY_D-2020-04-21.zip
+  wget https://github.com/zaproxy/zaproxy/releases/download/w2020-04-21/ZAP_WEEKLY_D-2020-04-21.zip
+  unzip ZAP_WEEKLY_D-2020-04-21.zip
       cd /root/
-    sudo ln -s /opt/ZAP_D-2020-04-21/zap.sh /usr/bin/zapnu11secur1ty
+    ln -s /opt/ZAP_D-2020-04-21/zap.sh /usr/bin/zapnu11secur1ty
 exit 0;
