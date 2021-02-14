440274903331a75c09e51b3b0362989a95a193e1
nu11secur1ty <venvaropt@gmail.com>
2021-02-14 09:33:19 +0200

    Update gadmrusna.pl

--- gadmrusna.pl
+++ gadmrusna.pl
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use diagnostics;
 
-print "Give the url...\n";
+print "Give the url, for example: http://domain.com/ or https://domain.com\n";
 chomp (my $URL_target = <STDIN>);
         my $zapnu11secur1ty_killer_auto = `sudo zapnu11secur1ty -quickurl $URL_target -quickout /home/nu11secur1ty/zapnu11secur1ty.xml`;
         system($zapnu11secur1ty_killer_auto);
