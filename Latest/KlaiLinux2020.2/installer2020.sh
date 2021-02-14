674cc3290d0363e66a1252713c95648391f0f017
nu11secur1ty <venvaropt@gmail.com>
2021-02-14 09:30:19 +0200

    Update installer2020.sh

--- Latest/KlaiLinux2020.2/installer2020.sh
+++ Latest/KlaiLinux2020.2/installer2020.sh
@@ -10,5 +10,5 @@ rm -rf /usr/bin/*zap*
   wget https://github.com/zaproxy/zaproxy/releases/download/w2021-02-08/ZAP_WEEKLY_D-2021-02-08.zip
   unzip ZAP_WEEKLY_D-2021-02-08.zip
       cd /root/
-    ln -s /opt/ZAP_WEEKLY_D-2021-02-08/zap.sh /usr/bin/zapnu11secur1ty
+    ln -s /opt/ZAP_D-2021-02-08/zap.sh /usr/bin/zapnu11secur1ty
 exit 0;
