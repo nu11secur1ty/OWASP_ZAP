199e091ee606345265faf3b59e8a62599e26a632
nu11secur1ty <venvaropt@gmail.com>
2021-02-14 10:06:17 +0200

    Update gadmrusna.pl

--- gadmrusna.pl
+++ gadmrusna.pl
@@ -3,16 +3,13 @@
 use strict;
 use warnings;
 use diagnostics;
-use File::HomeDir qw(home);
-use File::Spec::Functions qw(catfile);
-
-print "The path is ", catfile( home()) "\n";
+use Cwd qw(getcwd);
+my $H0me_DIR = getcwd;
 
 print "Give the url, for example: http://domain.com/ or https://domain.com\n";
 chomp (my $URL_target = <STDIN>);
         my $zapnu11secur1ty_killer_auto = `zapnu11secur1ty -quickurl $URL_target -quickout /home/nu11secur1ty/zapnu11secur1ty.xml`;
         system($zapnu11secur1ty_killer_auto);
-                my $home_DIR = `cd $HOME`;
-        my $chr0m_ = `google-chrome $home_DIR zapnu11secur1ty.xml`;
+        my $chr0m_ = `google-chrome $H0me_DIR zapnu11secur1ty.xml`;
         system($chr0m_);
 exit 0;
