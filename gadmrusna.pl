a1b2377748655c019bea9380ab53cabf0cb7b9f9
nu11secur1ty <venvaropt@gmail.com>
2021-02-14 10:48:03 +0200

    Update gadmrusna.pl

--- gadmrusna.pl
+++ gadmrusna.pl
@@ -10,7 +10,7 @@ my $H0me_DIR = getcwd;
 
 print "Give the url, for example: http://domain.com/ or https://domain.com\n";
 chomp (my $URL_target = <STDIN>);
-        my $zapnu11secur1ty_killer_auto = `zapnu11secur1ty -quickurl $URL_target -quickout $H0me_DIR zapnu11secur1ty.xml`;
+        my $zapnu11secur1ty_killer_auto = `zapnu11secur1ty -quickurl $URL_target -quickout $H0me_DIR/zapnu11secur1ty.xml`;
         system($zapnu11secur1ty_killer_auto);
         my $chr0m_ = `google-chrome $H0me_DIR zapnu11secur1ty.xml`;
         system($chr0m_);
