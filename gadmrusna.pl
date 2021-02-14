66656cfe085276a3d6f0e4e5e0ab0ac57e40552f
nu11secur1ty <venvaropt@gmail.com>
2021-02-14 09:55:29 +0200

    Update gadmrusna.pl

--- gadmrusna.pl
+++ gadmrusna.pl
@@ -8,6 +8,7 @@ print "Give the url, for example: http://domain.com/ or https://domain.com\n";
 chomp (my $URL_target = <STDIN>);
         my $zapnu11secur1ty_killer_auto = `zapnu11secur1ty -quickurl $URL_target -quickout /home/nu11secur1ty/zapnu11secur1ty.xml`;
         system($zapnu11secur1ty_killer_auto);
-        my $chr0m_ = `google-chrome zapnu11secur1ty.xml`;
+                my $home_DIR = `cd $HOME`;
+        my $chr0m_ = `google-chrome $home_DIR zapnu11secur1ty.xml`;
         system($chr0m_);
 exit 0;
